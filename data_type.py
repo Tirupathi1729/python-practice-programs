1fd0ab65b749549247f5106e69cc84ced3a52572
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-07-26 20:03:58 +0530

    data types

--- data_type.py
+++ data_type.py
@@ -1,2 +1,32 @@
-var_1=123456789876543456
-print(var_1+1)
+var_1=10
+var_2 =11
+print(type(var_1))
+print(var_1 + var_2)
+print(type(var_1))
+print(type(var_2))
+var_1=0o10
+var_2 =0x11
+print(var_1)
+print(var_2)
+print(type(var_1))
+print(type(var_2))
+
+
+#strings
+name_1 ="Tirupathi"
+name_2 ="Kakarla"
+
+print(type(name_1))
+print(name_1[0])
+
+#boolean
+
+var=True
+print(var)
+print(type(var))
+
+a=1
+b=2
+var=a>b
+print(var)
+
